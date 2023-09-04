5dc5404ba47a0237572c71b99df17ec9285e7f3c
Daojiuzahuopu <12907249+daojiuzahuopu@user.noreply.gitee.com>
2023-09-04 15:53:24 +0800

    ubuntu安装openmodelica依赖

--- README.md
+++ README.md
@@ -96,5 +96,6 @@
 
 ## 许可证
 
-本项目采用 [许可证名称](链接) 进行许可。详细信息请参阅许可证文件。
+本项目采用 [Apache License, Version 2.0](http://www.apache.org/licenses/LICENSE-2.0) 进行许可。详细信息请参阅 [许可证文件](LICENSE)。 
+
 ```
--- install_openmodelica.sh
+++ install_openmodelica.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+#####################################################################
+# Project Name: install_ububtu_openmodelica
+# Author: xudaojin
+# Email: 52725351@qq.com
+# Copyright (C) 2023-2028 xudaojin
+# 
+# Under the terms of the Apache License, Version 2.0 (hereinafter referred to as "License"),
+# you are not allowed to use this file unless complying with the provisions of the License.
+# You may obtain a copy of the License at: http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software distributed under the License
+# is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for specific language permissions and limitations. For questions, please contact the author for a copy of the License.
+#####################################################################
 
 # 更新系统软件包列表
 sudo apt-get update
